33ed08d7f584880c01d27635b3cb10e6be43ad71
Kazumi Inada <hello@nandenjin.com>
2019-09-14 09:06:41 +0900

    Add view for development

---
+++ src/dev.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  div.wrap
+    md-editor.editor(v-model="output")
+    code.preview
+      | {{ output }}
+</template>
+
+<script>
+import Vue from 'vue'
+import MdEditor from './md-editor.vue'
+
+export default Vue.extend({
+  components: {
+    MdEditor
+  },
+  data() {
+    return {
+      output: ''
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+html, body
+  margin: 0
+
+.wrap
+  display: flex
+  width: 100vw
+  height: 100vh
+
+  .editor, .preview
+    width: 50%
+    height: 100%
+    flex: 1 1 auto
+
+  .preview
+    background-color: #eee
+    white-space: pre-wrap
+</style>
--- src/md-editor.vue
+++ src/md-editor.vue
@@ -1,14 +1,48 @@
 <template lang="pug">
-  div.md-editor
-    | Hello
+  div.nandenjin_md-editor
+    div.toolbar
+    div.content(
+      ref="content"
+      contenteditable='true'
+      @input='onChange'
+    )
 </template>
 
 <script>
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    onChange() {
+      const value = this.$refs.content.innerHTML
+      this.$emit('input', value)
+    }
+  }
+})
 </script>
 
-<style lang="sass" scoped>
-.md-editor
+<style lang="sass">
+.nandenjin_md-editor
+  display: grid
+  grid-template-rows: 20px 1fr
   background-color: #fff
+
+  .toolbar
+    grid-row: 1
+
+  .content
+    grid-row: 2
+    outline: 0
+
+    font: normal 17px sans-serif
+    line-height: 1.8em
+
+    div
+      margin-bottom: .5em
 </style>
